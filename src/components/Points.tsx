4cfc18d26f15fe19501b96c60466eb29a073aeca
DocRAID <l01062506145@gmail.com>
2022-12-21 04:06:41 +0900

    mui-compo added

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -1,4 +1,17 @@
 import "./Points.css";
+import * as React from 'react';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
+import HotelIcon from '@mui/icons-material/Hotel';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
 interface ContainerProps {
   username: string;
 }
@@ -6,12 +19,89 @@ interface ContainerProps {
 const Points: React.FC<ContainerProps> = ({ username }) => {
   return (
     <div>
-      <div className="img">
-        <img src="https://cdn.discordapp.com/attachments/1012275291122585620/1037021491088334929/2022-11-02_001154.png"></img>
+      <div className="align">
+        <Timeline position="alternate" nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
+          <TimelineItem>
+            <TimelineOppositeContent
+              sx={{ m: 'auto 0' }}
+              align="right"
+              variant="body2"
+              color="text.secondary"
+            >
+              9:30 am
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot>
+                <FastfoodIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+              <Typography variant="h6" component="span">
+                Eat
+              </Typography>
+              <Typography>Because you need strength</Typography>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent
+              sx={{ m: 'auto 0' }}
+              variant="body2"
+              color="text.secondary"
+            >
+              10:00 am
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary">
+                <LaptopMacIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+              <Typography variant="h6" component="span">
+                Code
+              </Typography>
+              <Typography>Because it&apos;s awesome!</Typography>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineConnector />
+              <TimelineDot color="primary" variant="outlined">
+                <HotelIcon />
+              </TimelineDot>
+              <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+              <Typography variant="h6" component="span">
+                Sleep
+              </Typography>
+              <Typography>Because you need rest</Typography>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+              <TimelineDot color="secondary">
+                <RepeatIcon />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent sx={{ py: '12px', px: 2 }}>
+              <Typography variant="h6" component="span">
+                Repeat
+              </Typography>
+              <Typography>Because this is the life you love!</Typography>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
         <span className="result">
           현재 {username} 님의 상벌점은 20점 입니다
         </span>
       </div>
+      
     </div>
   );
 };

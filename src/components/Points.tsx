d02b4c201aa87a154c3dc0f5c3e75915f8f5f713
DocRAID <l01062506145@gmail.com>
2022-12-21 14:04:18 +0900

    points

--- src/components/Points.tsx
+++ src/components/Points.tsx
@@ -7,95 +7,85 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
+
 interface ContainerProps {
   username: string;
 }
 
 const Points: React.FC<ContainerProps> = ({ username }) => {
+  const data = [
+    {
+      'division': '성희롱',
+      'point': 10,
+      'accumulate': 10,
+      'issuer': '김진효',
+      'date': '2022-10-10'
+    },
+    {
+      'division': '성희롱',
+      'point': 10,
+      'accumulate': 10,
+      'issuer': '김진효',
+      'date': '2022-10-10'
+    },
+    {
+      'division': '성희롱',
+      'point': 10,
+      'accumulate': 10,
+      'issuer': '김진효',
+      'date': '2022-10-10'
+    },
+    {
+      'division': '성희롱',
+      'point': 10,
+      'accumulate': 10,
+      'issuer': '김진효',
+      'date': '2022-10-10'
+    },
+    {
+      'division': '전국대회 수상',
+      'point': -10,
+      'accumulate': 10,
+      'issuer': '김진효',
+      'date': '2022-10-10'
+    }
+  ]
+
+
   return (
     <div>
       <div className="align">
         <Timeline position="alternate" nonce={undefined} onResize={undefined} onResizeCapture={undefined}>
-          <TimelineItem>
-            <TimelineOppositeContent
-              sx={{ m: 'auto 0' }}
-              align="right"
-              variant="body2"
-              color="text.secondary"
-            >
-              9:30 am
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot>
-                <FastfoodIcon />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: '12px', px: 2 }}>
-              <Typography variant="h6" component="span">
-                Eat
-              </Typography>
-              <Typography>Because you need strength</Typography>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineOppositeContent
-              sx={{ m: 'auto 0' }}
-              variant="body2"
-              color="text.secondary"
-            >
-              10:00 am
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot color="primary">
-                <LaptopMacIcon />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: '12px', px: 2 }}>
-              <Typography variant="h6" component="span">
-                Code
-              </Typography>
-              <Typography>Because it&apos;s awesome!</Typography>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot color="primary" variant="outlined">
-                <HotelIcon />
-              </TimelineDot>
-              <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: '12px', px: 2 }}>
-              <Typography variant="h6" component="span">
-                Sleep
-              </Typography>
-              <Typography>Because you need rest</Typography>
-            </TimelineContent>
-          </TimelineItem>
-          <TimelineItem>
-            <TimelineSeparator>
-              <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
-              <TimelineDot color="secondary">
-                <RepeatIcon />
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: '12px', px: 2 }}>
-              <Typography variant="h6" component="span">
-                Repeat
-              </Typography>
-              <Typography>Because this is the life you love!</Typography>
-            </TimelineContent>
-          </TimelineItem>
+          {Object.values(data).map((log:any, index:number) => (
+            <TimelineItem key={index}>
+              <TimelineOppositeContent
+                sx={{m:'auto 0'}}
+                variant="body2"
+                >
+                  <div><span style={{ color: log.point < 0 ? '#ce2c2c' : '#04ad04' }}>{log.point > 0 ? '상점' : '벌점'} {Math.abs(log.point)}점</span></div>
+                  <div>총 <span style={{ color: '#2951c7' }}>{log.accumulate}점 </span></div>
+                </TimelineOppositeContent>
+                <TimelineSeparator>
+                <TimelineConnector/>
+                <TimelineDot style={{backgroundColor: log.point < 0 ? '#ce2c2c' : '#04ad04'}}>
+                  {
+                    log.point < 0 ? <RemoveIcon/> : <AddIcon/>
+                  }
+                </TimelineDot>
+                <TimelineConnector/>
+              </TimelineSeparator>
+
+              <TimelineContent sx={{py: '12px', px: 2}}>
+                <Typography variant="h6" component="span">
+                  <div className={'division'}>{log.division}</div>
+                </Typography>
+                <Typography><span className={'date'}>{log.date}</span></Typography>
+              </TimelineContent>
+            </TimelineItem>
+          ))}
         </Timeline>
         <span className="result">
           현재 {username} 님의 상벌점은 20점 입니다
